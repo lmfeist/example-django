9d088d52c8949d683d43017efb832b0443c3c87a
Leonard Feist <lmfeist@users.noreply.github.com>
2025-10-05 22:12:51 +0200

    Add secure proxy SSL header to settings.py

--- example_django/settings.py
+++ example_django/settings.py
@@ -73,6 +73,7 @@ ASGI_APPLICATION = 'example_django.asgi.application'
 
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
