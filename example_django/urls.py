98136a10932b029d431e2991853fcb20f50ff141
Leonard Feist <lmfeist@users.noreply.github.com>
2025-10-05 21:39:19 +0200

    Update URL configuration for Django auth module

--- example_django/urls.py
+++ example_django/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin, auth
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -23,5 +23,5 @@ urlpatterns = [
     path('time/', views.get_current_time, name='get_time'),
     path('greet/', views.greet, name='greet'),
     path('admin/', admin.site.urls),
-    path('accounts/', auth.urls),
+    path('accounts/', include("django.contrib.auth.urls")),
 ]
